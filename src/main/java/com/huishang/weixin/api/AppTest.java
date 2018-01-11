5d87061fe02b32461f0778631cce9c2f04ecc767
gongdaoshun <597841337@qq.com>
2018-01-11 12:55:44 +0800

    初始化仓库

---
+++ src/main/java/com/huishang/weixin/api/AppTest.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018 Baidu, Inc. All Rights Reserved.
+ */
+package com.huishang.weixin.api;
+
+/**
+ * TODO: 请添加描述
+ *
+ * @author gongdaoshun
+ * @date 2018/1/11
+ * @since 1.0.0
+ */
+public class AppTest {
+}
